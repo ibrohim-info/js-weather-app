ced7fd9112a22596b475ce526306883b6a035092
ibrohim <ahnazarovabdulla@gmail.com>
2022-01-16 07:49:23 +0300

    bismilloh

--- script.js
+++ script.js
@@ -22,5 +22,46 @@ function getResults(query) {
 
 
 function displayResults(weather){
-    console.log(weather)
+    console.log(weather);
+    let city = document .querySelector(".location .city");
+    city.innerHTML = `${weather.name}, ${weather.sys.country}`;
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerHTML = dateBuilder(now);
 }
+
+function dateBuilder(i){
+    let months = [
+        'January', 
+        'February', 
+        'March', 
+        'April', 
+        'May', 
+        'June', 
+        'Juiy', 
+        'August', 
+        'September', 
+        'October', 
+        'November', 
+        'December'
+    ];
+    let days = [
+        'Sunday', 
+        'Monday', 
+        'Tuesday', 
+        'Wednesday', 
+        'Thursday', 
+        'Friday', 
+        'Saturday'
+    ];
+
+    let day = days[i.getDay()];
+    let date = i.getDate();
+    let month = months[i.getMonth()];
+    let year = i.getFullYear();
+
+
+    return `${day} ${date} ${month} ${year}`;
+}
+
+
